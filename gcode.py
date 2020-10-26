42d9562dc14289c56266515e23e7a8fb27f40ae8
Jonas <tuberjonas@gmail.com>
2020-10-26 19:45:11 +0100

    Create gcode.py

---
+++ gcode.py
@@ -0,0 +1,31 @@
+# https://gist.github.com/mpilosov/92c6e7aea32345248504084c72a96926
+
+import serial
+import time
+
+# Open grbl serial port ==> CHANGE THIS BELOW TO MATCH YOUR USB LOCATION
+# GRBL operates at 115200 baud. Leave that part alone.
+s = serial.Serial('/dev/tty.usbmodem1811', 115200)
+
+# Open g-code file
+f = open('grbl.gcode', 'r')
+
+# Wake up grbl
+s.write("\r\n\r\n")
+time.sleep(2)   # Wait for grbl to initialize
+s.flushInput()  # Flush startup text in serial input
+
+# Stream g-code to grbl
+for line in f:
+l = line.strip()  # Strip all EOL characters for consistency
+print 'Sending: ' + l,
+s.write(l + '\n')  # Send g-code block to grbl
+grbl_out = s.readline()  # Wait for grbl response with carriage return
+print ' : ' + grbl_out.strip()
+
+# Wait here until grbl is finished to close serial port and file.
+raw_input("  Press <Enter> to exit and disable grbl.")
+
+# Close file and serial port
+f.close()
+s.close()
